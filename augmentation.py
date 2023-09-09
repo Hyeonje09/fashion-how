ee33ef35d9dade9bc719dea83715ec6fd45dd02b
Hyeonje09 <104563212+Hyeonje09@users.noreply.github.com>
2023-09-09 17:00:32 +0900

    Update augmentation.py

--- augmentation.py
+++ augmentation.py
@@ -1,18 +1,15 @@
 from torchvision import transforms
 from PIL import ImageFilter
 
-def gaussian_blur(image, kernel_size=5):
+def gaussian_blur(image, kernel_size=3):
     image_pil = image.filter(ImageFilter.GaussianBlur(kernel_size))
     return image_pil
 
 def flip(image):
     return transforms.functional.hflip(image)
 
-def plus_rotate(image, degrees=20):
-    return transforms.functional.rotate(image, degrees)
-
-def minus_rotate(image, degrees=-20):
-    return transforms.functional.rotate(image, degrees)
+def random_rotate(image, degrees=20):
+    return transforms.RandomRotation(degrees)(image)
 
 def apply_augmentation(image, augmentation_functions):
     augmented_images = [image]
